d22e7250c43fedf2235a2da9807c5b93fd3d69fa
mariotaku.lee@gmail.com <mariotaku.lee@gmail.com@54f9583e-b829-43be-82b0-c3a4c1089f2d>
2011-07-17 06:32:53 +0000

    based on 1.3.1

---
+++ openwnn-legacy/obj/local/armeabi/objs/wnndict/engine/ndbdic.o.d
@@ -0,0 +1,20 @@
+/home/mariotaku/工程/OpenWnn-legacy/obj/local/armeabi/objs/wnndict/engine/ndbdic.o: \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/engine/ndbdic.c \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_lib.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njx_lib.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_err.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_ext.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_dic.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njd.h
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_lib.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njx_lib.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_err.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_ext.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_dic.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njd.h:
---
+++ openwnn-legacy/obj/local/armeabi/objs/wnndict/engine/necode.o.d
@@ -0,0 +1,11 @@
+/home/mariotaku/工程/OpenWnn-legacy/obj/local/armeabi/objs/wnndict/engine/necode.o: \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/engine/necode.c \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_lib.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njx_lib.h \
+ /home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_ext.h
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_lib.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/njx_lib.h:
+
+/home/mariotaku/工程/OpenWnn-legacy/jni/libwnnDictionary/include/nj_ext.h:
